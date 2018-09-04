454b5d422cbec2439f52156c98af1f8eac5c72a8
Kunal1414 <42959552+Kunal1414@users.noreply.github.com>
2018-09-04 13:03:12 +0530

    Add files via upload

---
+++ HomeController.java
@@ -0,0 +1,21 @@
+package com.journaldev.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	public ModelAndView user() {
+		ModelAndView m = new ModelAndView("test");
+		m.addObject("message","hello");
+		return m;
+	}
+
+}
